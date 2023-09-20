7e603640190a8ce59bac796d31790ad9317c4c54
ahmedgaberfci <ahmedgaber2050@yahoo.com>
2023-09-20 14:13:39 +0300

    add new purc

--- purchase_request/models/purchase_order.py
+++ purchase_request/models/purchase_order.py
@@ -9,6 +9,7 @@ from odoo.exceptions import ValidationError
 class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
 
+
     purchase_types_id = fields.Many2one(comodel_name="purchase.types", string="PO Types", required=True)
 
     cfo_confirm = fields.Boolean(string="Supply Chain Approver", tracking=True, copy=False)
